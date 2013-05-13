f5e8a72113f815d60da23d4c8dedcc7c03596563
Dave Kierans <dkierans@gmail.com>
2013-05-13 13:40:53 +1000

    dev roll up

--- export-gmail.py
+++ export-gmail.py
@@ -132,11 +132,10 @@ def save(f, id, email_message, subject, date, body):
 
     placeholder = '?' if  'sqlite3' == DBA.__name__ else '%s'
 
-    print "XXX-->", body, "<--XXX"
-
     try:
         qs = "INSERT INTO %s (id, subject, dt, body, email) VALUES (%s,%s,%s,%s,%s)" % (t, placeholder, placeholder, placeholder, placeholder, placeholder)
         c.execute(qs, tuple)
+        print "LE ", c._last_executed
     except DBA.IntegrityError, e:
         " A fail due to unique constraint firing - ignore - this is OK "
         pass
