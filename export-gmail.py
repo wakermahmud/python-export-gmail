03251418348807a41662db225a8c1e0f836f1564
Dave Kierans <dkierans@gmail.com>
2013-05-17 17:37:42 +1000

    let's use the options parser instead

--- export-gmail.py
+++ export-gmail.py
@@ -214,7 +214,7 @@ if __name__ == "__main__":
     db = options.DATABASE
     save_in_db = options.SAVE
 
-    use_sqlite = (mu == None)
+    use_sqlite = (h == None)
     
     try:
         if not use_sqlite:
